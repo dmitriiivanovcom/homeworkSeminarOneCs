bc300742a9ba009f7d3e2f042df0f64f3b334868
Dmitrii Ivanov <114752708+dmitriiivanovcom@users.noreply.github.com>
2022-10-08 18:25:26 +0300

    Add files via upload
    second lesson

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿// first
+/*Console.WriteLine("Please, give me three-digit number.");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num >=100 && num < 1000)
+    {
+    int ld = ((num % 100) / 10);
+    Console.WriteLine($"last digit of {num} is {ld}");
+    }
+else
+    Console.WriteLine("Uncorrect input!");
+*/
+
+// second example.
+    //third from the end
+/*Console.WriteLine("Give me integer number.");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num >=100)
+    {
+    int tn = (num / 100 % 10);
+    Console.WriteLine($"Third digit of {num} is {tn}");
+    }
+else
+    Console.WriteLine("Uncorrect input!");
+*/
+
+    //third from the beginning
+/*Console.WriteLine("Give me integer number.");
+int num = Convert.ToInt32(Console.ReadLine());
+int temp = num;
+if(num > 1000)
+    do
+        temp = temp / 10;
+    while(temp > 1000);
+if(temp < 1000 && num >= 100)
+    {
+    temp = temp % 10;
+    Console.WriteLine($"Third digit if {num} is {temp}");
+    }
+else
+    Console.WriteLine("Its too small.");
+*/
+
+// third example
+/*
+Console.WriteLine("give me the day of the week number.");
+int day = Convert.ToInt32(Console.ReadLine());
+if(day > 0 && day < 6)
+    Console.WriteLine($"{day} is a weekday");
+if(day > 5 && day < 8)
+    Console.WriteLine($"{day} is outlet");
+if(day < 1 || day > 7)
+    Console.WriteLine("Wrong number!");
+*/
